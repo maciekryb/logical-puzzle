06a3ddc118af8d2c18198ee6683ae84ff299fbe8
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-18 14:13:07 +0100

    add handleClicks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./app.css";
 function App() {
   const [location, setLocation] = useState({
     box12: [1, 2],
-    box22: [2, 2],
+    box22: [5, 4],
     box11: [1, 1],
     box21: [2, 1],
   });
@@ -21,7 +21,45 @@ function App() {
   };
 
   const handleRestart = () => {
-    setIsActive({ box12: true, box22: false, box11: false });
+    setLocation({ box12: [1, 2], box22: [2, 2], box11: [1, 1], box21: [2, 1] });
+  };
+
+  const handleMoveRight = () => {
+    console.log("handleMoveRight");
+    // setLocation({ ...location, box12: [1, location.box12[0] + 1] });
+    let a = location.box12[0];
+    if (a + 1 < 3) {
+      setLocation({
+        ...location,
+        box12: [location.box12[0] + 1, location.box12[1]],
+      });
+    }
+  };
+
+  const handleMoveDown = () => {
+    console.log("handleMoveDown");
+    let a = location.box12[1];
+    if (a - 1 > 0) {
+      setLocation({ ...location, box12: [location.box12[0], location.box12[1] - 1] });
+    }
+  };
+
+  const handleMoveLeft = () => {
+    console.log("handleMoveLeft");
+
+    let a = location.box12[0];
+    if (a - 1 > 0) {
+      setLocation({ ...location, box12: [location.box12[0] - 1, location.box12[1]] });
+    }
+  };
+
+  const handleMoveUp = () => {
+    console.log("handleMoveUp");
+
+    let a = location.box12[1];
+    if (a + 1 < 3) {
+      setLocation({ ...location, box12: [location.box12[0], location.box12[1] + 1] });
+    }
   };
 
   return (
@@ -32,14 +70,22 @@ function App() {
       </button>
       <div className="App__game">
         <div className="App__game-box" id="box1" onClick={handleClick}>
-          {/* {location.box12} <br />
-          {isActive.box12.toString()} */}
-          <button className="App__game-box-button top">Góra</button>
+          {location.box12}
+          {/* {isActive.box12.toString()} */}
+          <button className="App__game-box-button top" onClick={handleMoveUp}>
+            Góra
+          </button>
           <br />
-          <button className="App__game-box-button left">Lewo</button>
-          <button className="App__game-box-button right">Prawo</button>
+          <button className="App__game-box-button left" onClick={handleMoveLeft}>
+            Lewo
+          </button>
+          <button className="App__game-box-button right" onClick={handleMoveRight}>
+            Prawo
+          </button>
           <br />
-          <button className="App__game-box-button bottom">Dół</button>
+          <button className="App__game-box-button bottom" onClick={handleMoveDown}>
+            Dół
+          </button>
         </div>
 
         {/* {isActive.box22 ? (
